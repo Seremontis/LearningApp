43bf4378c9caed22bca2aa934dd8ef45ed9a3a95
Patryk Wiśniewski <wisniewskii.patryk@o2.pl>
2022-01-06 13:53:45 +0100

    finish totally basic vesrion quiz

--- LearningApp/LearningApp/Controllers/LearningController.cs
+++ LearningApp/LearningApp/Controllers/LearningController.cs
@@ -1,8 +1,13 @@
 ﻿using EFDataAccesLibrary.DataAcces;
+using Extensions;
+using LearningApp.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace LearningApp.Controllers
@@ -10,7 +15,7 @@ namespace LearningApp.Controllers
     public class LearningController : BaseController
     {
         private readonly QuestionContext _db;
-
+        private const string excelLocalization = @"D:\Baza_pytan.xlsx";
         public LearningController(QuestionContext db)
         {
             _db = db;
@@ -18,11 +23,60 @@ namespace LearningApp.Controllers
 
         public IActionResult Index()
         {
-            var test =_db.Questions.ToList();
+            var vieModel = new List<QuizModel>();
+            //var test =_db.Questions.ToList();
 
-            return View();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (ExcelPackage xlPackage = new ExcelPackage(new FileInfo(excelLocalization)))
+            {
+                foreach (var myWorksheet in xlPackage.Workbook.Worksheets)
+                {
+                    var totalRows = myWorksheet.Dimension.End.Row;
+                    var totalColumns = myWorksheet.Dimension.End.Column;
+
+                    var sb = new StringBuilder();
+                    for (int rowNum = 2; rowNum <= totalRows; rowNum++)
+                    {
+                        var row = myWorksheet.Cells[rowNum, 1, rowNum, totalColumns].Select(c => c.Value == null ? string.Empty : c.Value.ToString()).ToList();
+
+                        if (!string.IsNullOrEmpty(row[1])) //&& row[1].ToLower().Contains("c#"))
+                        {
+                            vieModel.Add(new QuizModel()
+                            {
+                                Question = row[0],
+                                Type = row[1],
+                                Answerr = row[3],
+                                IsCorrect = bool.Parse(row[4])
+                            });
+                        }
+                    }
+                }
+            }
+            
+            vieModel.Shuffle();
+
+            var groupedViewModel = vieModel.GroupBy(x => x.Question).ToDictionary(x => x.Key, x => x.ToList());
+
+            foreach (var item in groupedViewModel)
+                item.Value.Shuffle();
+
+            return View(RandomDictionary(groupedViewModel));
         }
 
-
+        public Dictionary<string, List<QuizModel>> RandomDictionary(Dictionary<string, List<QuizModel>> quizModelNotSorted)
+        {
+            var randomDictionary = new Dictionary<string, List<QuizModel>>();
+            List<string> keyList = new List<string>(quizModelNotSorted.Keys);
+            Random rand = new Random();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                string randomKey = keyList[rand.Next(keyList.Count)];
+                keyList.Remove(randomKey);
+                randomDictionary.Add(randomKey, quizModelNotSorted[randomKey]);
+            }
+            return randomDictionary;
+        }
     }
 }
---
+++ LearningApp/LearningApp/ViewModel/QuizModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningApp.ViewModel
+{
+    public class QuizModel
+    {
+        public string Question { get; set; }
+        public string Type { get; set; }
+        public string Answerr { get; set; }
+
+        public bool IsCorrect { get; set; }
+
+    }
+}
